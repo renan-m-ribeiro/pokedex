75ae905a813e136f3726b67459b1a042211f3e90
Renan-m-ribeiro <renan.mendes013@gmail.com>
2021-06-16 23:11:11 -0300

    adds search and refactors code
    Co-authored-by: Jean Ribeiro <jeanmendesrib@gmail.com>

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 window.onload = function() {
     let index = 0;
+    const getById = (id) => document.getElementById(id);
+    const setBackground = (id, value) => getById(id).setAttribute('style', `background: ${value}`);
     updateCard(pokemons[index]);
 
     document.querySelectorAll("button").forEach(button => button.addEventListener("mouseup", function(event){
         if(event.target.id === "next") {
-            index >= pokemons.length -1 ? index = 0 : index++;
+            index >= pokemons.length - 1 ? index = 0 : index++;
         } else if(event.target.id === "prev") {
             index <= 0 ? index = pokemons.length -1 : index--;
         }
         updateCard(pokemons[index]);
     }));
-    
+
+    getById("search").addEventListener("keyup", function(event){
+        const foundIndex = pokemons.findIndex(pokemon => pokemon.name.includes(event.target.value.toLowerCase())) || 0;
+        updateCard(pokemons[foundIndex]);
+    })
+
     function updateBackground(pokemon){
-        document.getElementById("type1").setAttribute("style", `background-color: ${colorsType[pokemon.type1]}`);
-        document.getElementById("type2").setAttribute("style", `background-color: ${colorsType[pokemon.type2]}`);
-        if (pokemon.type2 != "none") {
-        document.getElementById("background").setAttribute("style", `background: linear-gradient( 135deg, ${colorsType[pokemon.type1]} 35% , ${colorsType[pokemon.type2]} 65% );`)
+        ["type1", "type2"].forEach(type => setBackground(type, colorsType[pokemon[type]]));
+        if (pokemon.type2) {
+            setBackground("background", `linear-gradient( 135deg, ${colorsType[pokemon.type1]} 35% , ${colorsType[pokemon.type2]} 65% );`)
         } else {
-            document.getElementById("background").setAttribute("style", `background: ${colorsType[pokemon.type1]}`);
-            document.getElementById("type2").setAttribute("style", `display: none`);
+            setBackground("background", colorsType[pokemon.type1]);
+            getById("type2").setAttribute("style", `display: none`);
         }
     }
-    function updateCard(pokemon) {
-        document.getElementById("card-front").innerHTML = `
-        <p class="text" id="id">${pokemon.id}</p>
-        <h1 class="text" id="name">${pokemon.name}</h1>
-        <img class="img" id="image" src="${pokemon.image}" alt="">
-        <div>
-            <p class="text ${pokemon.type1} box3" id="type1">${pokemon.type1}</p>
-            <p class="text ${pokemon.type2} box3" id="type2">${pokemon.type2}</p>
-        </div>
-        <div class="box2">
-            <p class="text2" id="description">${pokemon.description}</p>
-        </div>` 
 
-        document.getElementById("card-back").innerHTML = `
-        <p class="text" id="id">${pokemon.id}</p>
-        <h1 class="text" id="name">${pokemon.name}</h1>`
+    function updateCard(pokemon) {
+        getById("card-front").innerHTML = `
+            <p class="text" id="id">${pokemon.id}</p>
+            <h1 class="text name" id="name">${pokemon.name}</h1>
+            <img class="img" id="image" src="${pokemon.image}" alt="">
+            <div>
+                <p class="text ${pokemon.type1} box3" id="type1">${pokemon.type1}</p>
+                <p class="text ${pokemon.type2} box3" id="type2">${pokemon.type2}</p>
+            </div>
+            <div class="box2">
+                <p class="text2" id="description">${pokemon.description}</p>
+            </div>` 
+
+        getById("card-back").innerHTML = `
+            <p class="text" id="id">${pokemon.id}</p>
+            <h1 class="text" id="name">${pokemon.name}</h1>`
         updateBackground(pokemon)
     }
 }
--- pokemons.js
+++ pokemons.js
@@ -1,63 +1,58 @@
-  const pokemons = [ {
-    id: "#001",
-    name: "Bulbasaur",
-    image: "https://cdn2.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png",
-    type1: "grass",
-    type2: "poison",
-    description: "	A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon."
-    },
-
+  const pokemons = [
     {
-    id: "#002",
-    name: "Ivysaur",
-    image: "https://cdn2.bulbagarden.net/upload/thumb/7/73/002Ivysaur.png/250px-002Ivysaur.png",
-    type1: "grass",
-    type2: "poison",
-    description: "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs."
+      id: "#001",
+      name: "bulbasaur",
+      image: "https://cdn2.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png",
+      type1: "grass",
+      type2: "poison",
+      description: "	A strange seed was planted on its back at birth. The plant sprouts and grows with this Pokémon."
     },
-
     {
-    id: "#003",
-    name: "Venusaur",
-    image: "https://cdn2.bulbagarden.net/upload/thumb/a/ae/003Venusaur.png/250px-003Venusaur.png",
-    type1: "grass",
-    type2: "poison",
-    description: "The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight."
+      id: "#002",
+      name: "ivysaur",
+      image: "https://cdn2.bulbagarden.net/upload/thumb/7/73/002Ivysaur.png/250px-002Ivysaur.png",
+      type1: "grass",
+      type2: "poison",
+      description: "When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs."
     },
-
+    {
+      id: "#003",
+      name: "venusaur",
+      image: "https://cdn2.bulbagarden.net/upload/thumb/a/ae/003Venusaur.png/250px-003Venusaur.png",
+      type1: "grass",
+      type2: "poison",
+      description: "The plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight."
+    },
     {
       id: "#004",
-      name: "Charmander",
+      name: "charmander",
       image: "https://cdn2.bulbagarden.net/upload/thumb/7/73/004Charmander.png/250px-004Charmander.png",
       type1: "fire",
-      type2: "none",
+      type2: "",
       description: "Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail."
     },
-
     {
       id: "#005",
-      name: "Charmeleon",
+      name: "charmeleon",
       image: "https://cdn2.bulbagarden.net/upload/thumb/4/4a/005Charmeleon.png/250px-005Charmeleon.png",
       type1: "fire",
-      type2: "none",
+      type2: "",
       description: "When it swings its burning tail, it elevates the temperature to unbearably high levels."
     },
-
     {
       id: "#006",
-      name: "Charizard",
+      name: "charizard",
       image: "https://cdn2.bulbagarden.net/upload/thumb/7/7e/006Charizard.png/250px-006Charizard.png",
       type1: "fire",
       type2: "flying",
       description: "Spits fire that is hot enough to melt boulders. Known to cause forest fires unintentionally."
     },
-
     {
       id: "#007",
-      name: "Squirtle",
+      name: "squirtle",
       image: "https://cdn2.bulbagarden.net/upload/thumb/3/39/007Squirtle.png/250px-007Squirtle.png",
       type1: "water",
-      type2: "none",
+      type2: "",
       description: "After birth, its back swells and hardens into a shell. Powerfully sprays foam from its mouth."
     }
-]
+  ];
