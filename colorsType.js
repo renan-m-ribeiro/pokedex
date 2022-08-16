b9f0c2606b5be6138d1abe17c148bfc40e04726d
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-15 23:41:36 -0300

    colorType.js update

--- colorsType.js
+++ colorsType.js
@@ -4,5 +4,18 @@ const colorsType = {
     fire: "#f08030",
     none: "#fff",
     flying: "#a890f0",
-    water: "#6890f0"
+    water: "#6890f0",
+    normal: "#a8a878",
+    fighting: "#c03028",
+    electric: "#f8d030",
+    ground: "#e0c068",
+    psychic: "#f85888",
+    rock: "#b8a038",
+    ice: "#98d8d8",
+    bug: "#a8b820",
+    dragon: "#7038f8",
+    ghost: "#705898",
+    dark: "#705848",
+    steel: "#b8b8d0",
+    fairy: "#ffaec9"
 }
