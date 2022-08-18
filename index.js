6a4a4feb5dacb37973c7754fabfd42e24867c2d7
renan-m-ribeiro <renan.mendes013@gmail.com>
2022-08-17 23:29:16 -0300

    Add Front Image

--- index.js
+++ index.js
@@ -3,8 +3,32 @@ window.onload = function() {
     let indexMax = 151;
     const getById = (id) => document.getElementById(id);
     const setBackground = (id, value) => getById(id).setAttribute('style', `background: ${value}`);
-    pokemons(index)
-    updateCard(pokemon);
+
+   
+    if(index == 0) {
+        getById("card-show").innerHTML = `
+        
+        <input type="checkbox" id="switch" />
+        <button type="button" id="prev" class="button">&lt</button>
+        <img class="img" id="frontImage" src="src/pokedex.png" alt="">
+            <label id="label" class="flip-container" hidden for="switch" >
+                <div class="flip-container">
+                    <div class="flipper">
+                        <div class="front">
+                            <div id="card-front" class="box"></div>
+                        </div>
+                        <div class="back">
+                            <div id="card-back" class="box"></div>
+                        </div>
+                    </div>
+                </div>
+            </label>
+        
+        <button type="button" id="next" class="button">&gt</button>`
+    } else {
+        getById("frontImage").setAttribute(hidden, true);
+        getById("label").setAttribute(hidden, false);
+    }
 
     document.querySelectorAll("button").forEach(button => button.addEventListener("mouseup", async function(event){
         if(event.target.id === "next") {
@@ -15,6 +39,7 @@ window.onload = function() {
         await pokemons(index)
         updateCard(pokemon);
     }));
+
 
     /*getById("search").addEventListener("keyup", function(event){
         event.preventDefault();
